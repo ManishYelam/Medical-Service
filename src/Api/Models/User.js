e1ca1a95713370d0bfb37bb9db0ff8242ffbb021
Manish-Yelam <manish.ovtpl@gmail.com>
2024-10-04 22:49:06 +0530

    6

--- src/Api/Models/User.js
+++ src/Api/Models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../../config/Database/db.config');
+const { sequelize } = require('../../Config/Database/db.config');
 
 const User = sequelize.define('User', {
     id: {
