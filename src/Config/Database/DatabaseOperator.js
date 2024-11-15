414c53651cfcc15396f2eb55fc6a5e230545bb78
Manish-Yelam <manish.ovtpl@gmail.com>
2024-11-15 12:37:21 +0530

    74

--- src/Config/Database/DatabaseOperator.js
+++ src/Config/Database/DatabaseOperator.js
@@ -2,35 +2,52 @@ const { getInstanceByKey } = require('./departmentMapping');
 const models = require('./centralModelLoader');
 
 const DatabaseOperator = async (health_id, tableName) => {
+    const data = {};
+
     const deptKey = getInstanceByKey(health_id);
-    // console.log('Extracted department key:', deptKey);
-    if (!deptKey) {
-        return `Invalid department code.`;
-    }
+    if (!deptKey) { return { error: 'Invalid department code.' }; }
 
     const deptModel = models[deptKey];
-    // console.log('Retrieved department model:', deptModel);
-    if (!deptModel) {
-        return `No model found for the specified department.`;
-    }
+    if (!deptModel) { return { error: 'No model found for the specified department.' }; }
 
     const deptModelTable = deptModel[tableName];
-    // console.log(deptModelTable);
-    if (!deptModelTable) {
-        return `${tableName} model not found in department model.`;
-    }
+    if (!deptModelTable) { return { error: `${tableName} model not found in department model.` }; }
 
-    // try {
-    //     console.log(`Accessing ${tableName} model for department: ${deptKey}`);
+    data[tableName] = { deptKey, deptModel, deptModelTable, };
 
-    //     // Example query: Fetch all records from the specified table
-    //     const result = await deptModelTable.findAll();
-    //     console.log(`${tableName} data for ${deptKey}:`, result); 
-    // } catch (error) {
-    //     console.error(`Error accessing the ${tableName} model for department ${deptKey}:`, error.message);
-    // }
+    return data && console.log(data.User);
 };
 
-DatabaseOperator('MEDSRV437293', 'User');
+DatabaseOperator('PHARMA437293', 'User');
 
-module.exports = { DatabaseOperator }
+module.exports = { DatabaseOperator };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  // Loop through each table name provided and access the model dynamically
+//  const deptModelTables = {};
+//  const tableNames = deptModelTable;
+//  for (const tableName of tableNames) {
+//      const deptModelTable = deptModel[tableName];
+//      if (!deptModelTable) {
+//          return { error: `${tableName} model not found in department model.` };
+//      }
+//      deptModelTables[tableName] = deptModelTable;
+//  }
+
+//  // Store the retrieved models information
+//  data[deptKey] = deptModelTables;
