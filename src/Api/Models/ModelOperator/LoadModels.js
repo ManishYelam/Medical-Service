277c258c884e77169dd013d417d446a58674faf7
Manish-Yelam <manish.ovtpl@gmail.com>
2024-12-01 09:43:37 +0530

    109

--- src/Api/Models/ModelOperator/LoadModels.js
+++ src/Api/Models/ModelOperator/LoadModels.js
@@ -5,14 +5,14 @@ let globalModels = {};
 async function loadModels(health_id) {
   if (!globalModels[health_id]) {
     const tbl = await DatabaseOperator(health_id)
-       
+
     globalModels[health_id] = {
-        User:tbl.User.deptModel.User,
-        UserLog:tbl.UserLog.deptModel.UserLog,
-        Role:tbl.Role.deptModel.Role,
-        Permission:tbl.Permission.deptModel.Permission,
-        RolePermissions:tbl.RolePermissions.deptModel.RolePermissions,
-        Department:tbl.Department.deptModel.Department,
+      User: tbl.User.deptModel.User,
+      UserLog: tbl.UserLog.deptModel.UserLog,
+      Role: tbl.Role.deptModel.Role,
+      Permission: tbl.Permission.deptModel.Permission,
+      RolePermissions: tbl.RolePermissions.deptModel.RolePermissions,
+      Department: tbl.Department.deptModel.Department,
     };
   }
   return globalModels[health_id];
